7d097e7803888efa98cfc6b52fc31c91ef2e10ef
Kato <kldiashvilikhatuna@gmail.com>
2023-01-27 22:48:54 +0400

    update

--- main.js
+++ main.js
@@ -139,15 +139,43 @@ function LinkedLists(){
         }
     }
 
+    this.find=function(fn){
+        let start=firstNode;
+        while(start!==null){
+            if(fn(start.value)){
+                return start.value;
+            }
+            start=start.next;
+        }
+    }
+
+    this.filter=function(fn){
+        let filtered =  new LinkedLists();
+        this.forEach( e =>{
+            if(fn(e)){
+                filtered.push(e)
+            }
+        });
+
+        return filtered;
+    }
 
 
+    this.map=function(fun){
+        let newNode=new LinkedLists();
+        this.forEach(function(e){
+            newNode.push(e);
+        });
+
+        return newNode;
+    }
 
 }
 
-let nodes=new LinkedLists();
-nodes.push(1);
+let nodes=new LinkedLists()
 nodes.push(2);
-nodes.push(3);
+nodes.push(4);
+nodes.push(6);
 nodes.unshift(-1);
 nodes.unshift(-2);
 nodes.unshift(-3);
@@ -165,15 +193,42 @@ nodes.shift();
 nodes.shift();
 nodes.shift(); */
 //console.log(nodes.includes(3));
+//let newList=new LinkedLists()
 
-console.log(nodes.remove(2));
+//nodes = new LinkedLists();
+
+/* nodes.push({id:1,name:"khato"});
+nodes.push({id:2,name:"Genadi"});
+nodes.push({id:3,name:"Geno"});
+nodes.push({id:4,name:"Khatuna"});
+nodes.push({id:5,name:"kunwula"});
+nodes.push({id:6,name:"lasha"});*/
+
+let filter=nodes.filter(el=>{
+    return el % 2 === 0;
+})
+
+filter.forEach(e => console.log(e));
+console.log(filter.length);
+
+let map=nodes.map(e => {
+     return e*3
+    });
+
+map.forEach(e=> console.log(e));
+console.log(map.length);
 
 
+/* let find=nodes.find(e=>{
+     return e > 2
+    });
+console.log(find);
+console.log(nodes.remove(1));
 
 console.log(nodes.toArray());
-nodes.forEach(e => console.log(e));
+nodes.forEach(e => console.log(e)); */
 /* console.log("Let Try reverse!");
 nodes.forEachReverse((e,i)=>{
     console.log( e + " " + " index is: "+ " " + i)
 }); */
-console.log(nodes.length);
+//console.log(nodes.length);
